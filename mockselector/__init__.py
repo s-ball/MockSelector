c902053a6bcf111503060491777a1af83f92750d
s-ball <s-ball@laposte.net>
2020-08-14 22:23:49 +0200

    Fix a wrong version import.

--- mockselector/__init__.py
+++ mockselector/__init__.py
@@ -2,9 +2,9 @@
 
 from .selector import MockSelector, MockSocket, ListenSocket
 try:
-    from .version import __version__
+    from .version import version as __version__
 except ImportError:
     # be conservative if version.py could not be generated
-    # as it may happen on some CI platforms...
+    # as it might happen on some CI platforms...
     __version__ = '0.0.0'
 __all__ = [MockSocket, MockSelector, ListenSocket]
